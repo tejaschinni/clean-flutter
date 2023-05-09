713bf2511d13f299113cf51aafd15c7b57ccac51
Tejas Chinni <58680948+tejaschinni@users.noreply.github.com>
2023-05-09 22:19:10 +0530

    Initialize 2.4_scaffold_material_app

---
+++ 2_flutter/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(appBarTheme: const AppBarTheme(backgroundColor: Colors.amber)),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Flutter Basics")),
+        body: const Center(
+          child: Text("Hello World!"),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () => debugPrint('clicked'), child: const Icon(Icons.ac_unit)),
+      ),
+    );
+  }
+}

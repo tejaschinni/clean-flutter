b6df9c7087e36e36532c37e9296f94a20a289928
Tejas Chinni <58680948+tejaschinni@users.noreply.github.com>
2023-05-09 22:19:39 +0530

    Initialize 5.13_create_todo_entry

---
+++ 5_todo_app/lib/2_application/pages/detail/view_states/todo_detail_loaded.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:todo_app/1_domain/entities/unique_id.dart';
+import 'package:todo_app/2_application/components/todo_entry_item/todo_entry_item.dart';
+import 'package:todo_app/2_application/pages/create_todo_entry/create_todo_entry_page.dart';
+
+class ToDoDetailLoaded extends StatelessWidget {
+  const ToDoDetailLoaded({
+    super.key,
+    required this.collectionId,
+    required this.entryIds,
+  });
+
+  final List<EntryId> entryIds;
+  final CollectionId collectionId;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Stack(
+          children: [
+            ListView.builder(
+              itemCount: entryIds.length,
+              itemBuilder: (context, index) => ToDoEntryItemProvider(
+                collectionId: collectionId,
+                entryId: entryIds[index],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: FloatingActionButton(
+                key: const Key('create-todo-entry'),
+                onPressed: () {
+                  context.pushNamed(
+                    CreateToDoEntryPage.pageConfig.name,
+                    extra: collectionId,
+                  );
+                },
+                child: const Icon(Icons.add_rounded),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

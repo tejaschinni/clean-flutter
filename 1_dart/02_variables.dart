2eada9efd2c7893d37486c9987aecd52eb23d4da
Tejas Chinni <58680948+tejaschinni@users.noreply.github.com>
2023-05-09 22:19:14 +0530

    Initialize 2.10_media_query

---
+++ 1_dart/02_variables.dart
@@ -0,0 +1,10 @@
+void main(){
+
+  int outputInt = 2;  // initialization
+
+  print('outputInt: $outputInt.');
+
+  late int lateOutputInt; // declaration
+  lateOutputInt = 42;   // allocation
+  print('lateOutputInt: $lateOutputInt');
+}

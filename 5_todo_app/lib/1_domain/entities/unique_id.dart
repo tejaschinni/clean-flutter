dc1226c83332e72f4309a7ab7f55c12e8bf887b4
Tejas Chinni <58680948+tejaschinni@users.noreply.github.com>
2023-05-09 22:19:38 +0530

    Initialize 5.12_create_todo_collection

---
+++ 5_todo_app/lib/1_domain/entities/unique_id.dart
@@ -0,0 +1,43 @@
+import 'package:equatable/equatable.dart';
+import 'package:uuid/uuid.dart';
+
+class UniqueId with EquatableMixin {
+  const UniqueId._(this.value);
+
+  final String value;
+
+  factory UniqueId() {
+    return UniqueId._(const Uuid().v4());
+  }
+
+  factory UniqueId.fromUniqueString(String uniqueString) {
+    return UniqueId._(uniqueString);
+  }
+
+  @override
+  List<Object?> get props => [value];
+}
+
+class CollectionId extends UniqueId {
+  const CollectionId._(String value) : super._(value);
+
+  factory CollectionId() {
+    return CollectionId._(const Uuid().v4());
+  }
+
+  factory CollectionId.fromUniqueString(String uniqueString) {
+    return CollectionId._(uniqueString);
+  }
+}
+
+class EntryId extends UniqueId {
+  const EntryId._(String value) : super._(value);
+
+  factory EntryId() {
+    return EntryId._(const Uuid().v4());
+  }
+
+  factory EntryId.fromUniqueString(String uniqueString) {
+    return EntryId._(uniqueString);
+  }
+}
